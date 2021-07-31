6ab2c578f64c2eea029b9c0eae3a900353669726
fayalivity <rastafayalion@gmail.com>
2021-07-31 11:50:09 +0200

    escalier

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-#tortue
-print("tortue")
+import turtle
+
+def escalier(taille, nb):
+    for i in range(0, nb):
+        t.forward(taille)
+        t.left(90)
+        t.forward(taille)
+        t.right(90)
+    t.forward(taille)
+
+
+# 5 marches de 30*30px
+nb_marche = input("combien de marche ? ")
+taille_marche = input("quelle taille de marche ?")
+t = turtle.Turtle()
+escalier(int(taille_marche), int(nb_marche))
+
+turtle.done()
